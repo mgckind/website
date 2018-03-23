551fa483629b7fe97631a994f7e564ba75c33ee0
Matias Carrasco Kind <mgckind@gmail.com>
2018-03-22 23:48:01 -0500

    fix routes

---
+++ research/load.js
@@ -0,0 +1,34 @@
+    var app = document.querySelector('#app');
+    app.baseUrl = '/';
+
+      document.addEventListener('WebComponentsReady', function() {
+        var tabs = document.querySelector('paper-tabs');
+            var temp= $(window).width();
+            if (temp < 700){
+            tabs.style.visibility='hidden';
+            }
+            else{
+            tabs.style.visibility='visible';
+            };
+
+        tabs.selected="4";
+        //app.route="/";
+        var menu = document.querySelector('paper-menu');
+        //var appr = document.querySelector('app-route');
+        //appr.addEventListener('route-changed', function(){
+        //    console.log('1234567');
+        //});
+        var drawer = document.querySelector('paper-drawer-panel');
+        menu.addEventListener('iron-activate', function() {
+            drawer.closeDrawer();
+        });
+        window.addEventListener("resize", function(){
+            var temp= $(window).width();
+            if (temp < 700){
+            tabs.style.visibility='hidden';
+            }
+            else{
+            tabs.style.visibility='visible';
+            };
+            });
+        });
